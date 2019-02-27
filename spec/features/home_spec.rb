18e03930c9137cdc898745326cd57a9f90d6d8de
Dmitry Rocha <dmitryrck@gmail.com>
2019-02-27 20:59:27 +1300

    Add missing test case when user changes the ISBN.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -11,6 +11,20 @@ describe "Home" do
     expect(page).to have_content "Complete ISBN: 9780143007234"
   end
 
+  it "should hide 'Complete ISBN' message if user changes the isbn", js: true do
+    visit "/"
+
+    fill_in "ISBN", with: "978014300723"
+
+    click_on "Get digit"
+
+    expect(page).to have_content "Complete ISBN: 9780143007234"
+
+    fill_in "ISBN", with: "123"
+
+    expect(page).not_to have_content "Complete ISBN"
+  end
+
   it "should show errors", js: true do
     visit "/"
 
